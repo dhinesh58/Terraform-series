b31413e80f213d2f8c77be1421cffa669fb7deae
Dhinesh <157783841+dhinesh58@users.noreply.github.com>
2025-04-28 15:59:12 +0530

    Delete Day2-Variables directory

--- Day2-Variables/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-name = "Test-RG"
-location = "centralindia"
--- Day2-Variables/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-    type = string
-    description = "value for the Name "
-}
-
-variable "location" {
-  type = string
-  description = "value for the resource location"
-}
